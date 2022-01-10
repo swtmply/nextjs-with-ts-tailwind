19ec3f41d45754615694083fe03084b785fd33c9
John Allen Delos Reyes <delosreyesjohnallen@gmail.com>
2022-01-10 19:06:10 +0800

    remove custom hook sample

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,26 @@
 import type { NextPage } from 'next';
+import Link from 'next/link';
 import Layout from '../components/Layout';
-import useDocument from '../hooks/useDocument';
 
 const Home: NextPage = () => {
-  const { doc, handleFileSelect, ref } = useDocument();
-
   return (
     <Layout title="Home Page">
-      <div className="flex flex-col gap-8 ">
-        <h1>Document File to HTML</h1>
-        <input type="file" onChange={handleFileSelect} />
-        <div
-          ref={ref}
-          className="prose prose-strong:text-2xl max-w-none prose-a:text-blue-500 hover:prose-a:text-blue-600"
-        >
-          {doc}
-        </div>
+      <div className="flex flex-col gap-2 items-center">
+        <h1 className="font-bold text-3xl">
+          NextJS - Typescript Boilerplate with TailwindCSS
+        </h1>
+        <code className="py-1 px-2 bg-gray-100 rounded">
+          Edit <span className="font-bold"> pages/index.tsx </span> to start
+          creating your own app.
+        </code>
+        <p>
+          Check out the code at Github:{' '}
+          <Link href={'https://github.com/swtmply/nextjs-with-ts-tailwind'}>
+            <a className="underline text-blue-500" target="_blank">
+              https://github.com/swtmply/nextjs-with-ts-tailwind
+            </a>
+          </Link>
+        </p>
       </div>
     </Layout>
   );
